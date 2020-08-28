db2ebf2a22133f721502149f9cd6c97072a6f3bb
lcplreaper <56055923+lcplreaper@users.noreply.github.com>
2020-08-28 15:18:54 -0400

    h1z1-worlds (US)
    US worlds list for H1Z1 Emulator Project

---
+++ h1z1-worlds-list (US)/The Stronghold (Whitelist)/ZoneClient.js
@@ -0,0 +1,14 @@
+const h1serv = require("h1z1-server");
+
+var Zone = new h1serv.ZoneClient(
+  "127.0.0.1",
+  1117,
+  "The Stronghold (Whitelist)",
+  2,
+  "ok",
+  "udp9",
+  "0.2",
+  6782
+);
+
+Zone.connect();
