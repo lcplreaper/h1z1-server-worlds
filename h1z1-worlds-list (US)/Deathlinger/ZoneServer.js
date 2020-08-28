07b88adc7b906e7fd771d5fd82ece594caf9b23d
lcplreaper <56055923+lcplreaper@users.noreply.github.com>
2020-08-28 15:37:23 -0400

    Update ZoneServer.js

--- h1z1-worlds-list (US)/Deathlinger/ZoneServer.js
+++ h1z1-worlds-list (US)/Deathlinger/ZoneServer.js
@@ -1,5 +1,5 @@
 const h1serv = require("h1z1-server");
 
-var Zone = new h1serv.ZoneServer(1117, "Deathlinger", false);
+var Zone = new h1serv.ZoneServer(1123, "Deathlinger", false);
 
 Zone.start();
